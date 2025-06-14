0e644123977eab988c6d1b8d7fab15765e61b3f5
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2025-06-14 12:25:29 +0200

    help update

--- commands/help-slash.js
+++ commands/help-slash.js
@@ -8,13 +8,14 @@ module.exports = {
     async execute(interaction) {
         const embed = new EmbedBuilder()
         .setTitle('BallIdentifier')
-        .setFooter({ text: `v${version}` })
+        .setFooter({ text: `v${version} by @meffiu` })
         .setColor('#6839A6')
         .addFields(
             { name: 'Supported Bots', value: '- Ballsdex\n- DynastyDex\n- Empireballs\n- HistoryDex' },
             { name: 'How to Use', value: 'Right-click on a message with an image from one of the supported bots and select "Identify" (image below)' },
-            { name: 'More Information', value: '[GitHub Repository]()\n[Website](https://ballidentifier.xyz)' },
+            { name: 'More Information', value: '[GitHub Repository](https://github.com/Meff1u/BallIdentifier-bot)\n[Website](https://ballidentifier.xyz)' },
         )
+        .setImage('https://github.com/Meff1u/BallIdentifier-bot/blob/main/assets/tutorial.png?raw=true')
 
         await interaction.reply({ embeds: [embed], ephemeral: true });
     },
